6b6f2f87acb2436ae650ecb8ad98b39266a6ce14
Shrushti Kadam <kadamshrush@gmail.com>
2024-06-10 17:51:41 +0530

    Add files via upload

---
+++ array_syntax.c
@@ -0,0 +1,10 @@
+char c_arr[5]; //allocate contigious block of 5 chars and name it as c_arr
+short s_arr[3]; //allocate contigious block of 3 short ints and name it as s_arr.
+int ia[8]; //allocate contigious block of 4 single precision floating point
+float fa[4]; //allocte contigious block of 4 single floating point
+             // numbers and name it as lnga
+
+int main(void){
+    return(0);
+
+} 
---
+++ puts_demo.c
@@ -0,0 +1,11 @@
+int puts(const char*);
+
+int main(void){
+    puts("MASTERCLASS IN C : SESSION 037");
+    return(0);
+
+}
+
+//Build command
+//>cl /Fe:app.exe puts_demo.c
+//app.exe 
